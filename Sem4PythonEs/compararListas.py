9b596730e19079a48692a1517e12812ed7c73f91
Daniel Felipe Mejía Ríos <dmejiari@unal.edu.co>
2022-08-31 10:31:13 -0500

    Semana 4 estructuras de datos

---
+++ Sem4PythonEs/compararListas.py
@@ -0,0 +1,8 @@
+a = [1,2,3]
+b = [1,2,3]
+c = [1,2,3,4]
+
+print(a == b)
+print(a == c)
+print(a > c)
+print(c > a)
---
+++ Sem4PythonEs/operador de corte.py
@@ -0,0 +1,11 @@
+cad = "Manizales, ciudad de las puertas abiertas"
+print(cad[0:9])
+print(cad[11:17])
+print(cad[:9])
+
+print("Caracteres de las posiciones pares")
+print(cad[0:len(cad):2])
+print("Caracteres de las posiciones impares")
+print(cad[1:len(cad):2])
+print("manera simple de escribir la cadena al reves")
+print(cad[::-1])
